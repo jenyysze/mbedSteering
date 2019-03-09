408c41964ece3731c7d2a809396dadd84c533c90
Spencer Perkins <hello@spencerperkins.me>
2019-03-09 01:04:27 -0800

    Made all inputs pull down, tested and working

--- src/pins.h
+++ src/pins.h
@@ -6,39 +6,38 @@
 #ifndef PINS_H
 #define PINS_H
 
-// extern CAN can(p30, p29,1000000); // Actual pins
-extern CAN can(p9, p10, 1000000); // Use for testing (current wiring)
+extern CAN can(p30, p29,1000000); // Actual pins
+// extern CAN can(p9, p10, 1000000); // Use for testing (current wiring)
 extern Serial pc(USBTX, USBRX); // To print to computer terminal, for testing
 
 // Dials for throttle controls
-DigitalIn loRangePin(p15, PullUp); // dial1
-DigitalIn midRangePin(p16, PullUp); //dial2
-DigitalIn highRangePin(p17, PullUp); //dial3
-DigitalIn constantVelocityPin(p22, PullUp); //dialB
-DigitalIn constantPowerPin(p23, PullUp); //dialA
+DigitalIn loRangePin(p15, PullDown); // dial1
+DigitalIn midRangePin(p16, PullDown); //dial2
+DigitalIn highRangePin(p17, PullDown); //dial3
+DigitalIn constantVelocityPin(p22, PullDown); //dialB
+DigitalIn constantPowerPin(p23, PullDown); //dialA
 
 // Unused dial inputs
-DigitalIn dial4(p18, PullUp); //dial4
-DigitalIn dial5(p19, PullUp); //dial5
+DigitalIn dial4(p18, PullDown); //dial4
+DigitalIn dial5(p19, PullDown); //dial5
 
 DigitalIn rangePins[] = {loRangePin, midRangePin, highRangePin}; // [lo, mid, high]
 DigitalIn modePins[] = {constantVelocityPin, constantPowerPin}; // [constVel, constPow]
 CanThrottle throttle(p20,throttleID, rangePins, modePins); 
 
-
 // CanItems
 CanButton indicatorR(p5, indicatorRID);
 CanButton indicatorL(p6, indicatorLID);
 CanButton headlights(p8, headlightsID);
-// CanButton wiper(p9, wiperID); // Comment this out for testing (w/ current wiring)
-// CanButton harards(p10, hazardsID); // Comment this out for testing (w/ current wiring)
+CanButton wiper(p9, wiperID); // Comment this out for testing (w/ current wiring)
+CanButton hazards(p10, hazardsID); // Comment this out for testing (w/ current wiring)
 CanButton horn(p24, hornID);
 
 // TODO: Add screen pins
-DigitalIn motorDeactivated(p7, PullUp);
-DigitalIn deadManSwitch(p21, PullUp);
+DigitalIn motorActivated(p7, PullDown);
+DigitalIn deadManSwitch(p21, PullDown);
 
-Serial serial(p28,p27); // For communication with motor controller over UART (TX, RX)
+//Serial serial(p28,p27); // For communication with motor controller over UART (TX, RX)
 
 #endif
 
